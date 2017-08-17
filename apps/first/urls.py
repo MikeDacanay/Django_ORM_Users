fc02e024f3e7f22bb78d11c44d48fba82f0a8ba2
Mike Dacanay <migd64@gmail.com>
2017-08-16 22:05:00 -0700

    need to add validations

--- apps/first/urls.py
+++ apps/first/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
   url(r'^$', views.index),
-  url(r'new$', views.new)
+  url(r'new$', views.new),
+  url(r'add$', views.add),
+  url(r'(?P<idx>\d+)$', views.show),
+  url(r'(?P<idx>\d+)/edit$', views.edit),
+  url(r'(?P<idx>\d+)/update$', views.update)
 ]
--- apps/first/views.py
+++ apps/first/views.py
@@ -2,10 +2,44 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 
+from models import *
 # Create your views here.
 def index(request):
-	return render(request,'first/index.html')
+	users_data = {
+		"users_database": Users.objects.all()
+	}
+	# print Users.objects.values().all()
+	return render(request,'first/index.html',users_data)
 
 def new(request):
-	print "hello"
-	return redirect('/users')
+	return render(request,'first/new.html')
+
+def add(request):
+	x = request.POST['firstname_form']
+	y = request.POST['lastname_form']
+	z = request.POST['email_form']
+	Users.objects.create(first_name = x, last_name = y, email= z)
+	return redirect('/users/')
+
+def show(request, idx):
+	user_show={
+		"show": Users.objects.get(id=idx)
+	}
+	return render(request,'first/show.html',user_show)
+
+def edit(request, idx):
+	user_edit={
+		"edit": Users.objects.get(id=idx)
+	}
+	return render(request,'first/edit.html', user_edit)
+
+def update(request,idx):
+	x = request.POST['firstname_form']
+	y = request.POST['lastname_form']
+	z = request.POST['email_form']
+	user = Users.objects.get(id=idx)
+	user.first_name=x
+	user.last_name=y
+	user.email=z
+	user.save()
+	return redirect('/users/')
